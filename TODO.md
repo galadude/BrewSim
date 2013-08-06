2513215cafcbb8e8a87ba7e3f56e809e90b5f217
Galadude <michael@galanakis.dk>
2013-08-07 01:18:30 +0200

    beginning the command parser, needs to be changed though

--- TODO.md
+++ TODO.md
@@ -1,9 +1,16 @@
 TODO
 ======
+# Right now
+ **idea** | **status**
+ --- | --- 
+use dirs were appropriate | not started
+parse using verb/noun/quantity system | not started 
 
 # Short-term
  **idea** | **status**
  --- | --- 
 loans | not started
+sql beer | not started 
+add help | not started
 
 
--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 import random
+import sys
 
 # The resources owned by the player
+exit = False
 money = 1000
 hops = 0
 malt = 0
 beer = 0
-production_level = 1
+production_slots = 10
 
 class Beer:
 	def __init__(self,name,hops,malt,price,turns):
@@ -13,7 +15,7 @@ class Beer:
 		self.hops = hops
 		self.malt = malt
 		self.price = price 
-		self.turns = turns # Turns until finished 
+		self.turns = turns # Turns until finished
 
 # The base price of hops and malt
 prices = [2,5]
@@ -23,21 +25,107 @@ beers = [
 		Beer('Dubble',2,2,80,10),
 		Beer('Generic Lager',1,1,15,2)
 	]
-
+inventory = [0,0,0]
 	 
+queue = [[]] # beer being brewed
+
+def turn():
+	generate_prices()
+
+	new_queue = [[]]
+	for [name,turns] in queue:
+		new_queue.add([name,turns-1])
+	queue = new_queue	
+
+def brew(beer,quantity):
+	for row in range(0,quantity):
+		queue.add([beer.name,beer.turns])
+
 def generate_prices():
 	for price in prices:
 		# I'd like this to run on a normal distribution instead
 		rand_int = random.randrange(-1,1)		
 		price += price * rand_int
 
-def produce_beer():
-	hops -= 50 * production_level
-	malt -= 100 * production_level
-	beer += 100 * production_level
-
 def parse_command(cmd):
 	cmd_lst = cmd.split(' ')
-	for word in cmd_lst:
-		print word	
-parse_command('asdf asdf asfd')
+	global money
+	global hops
+
+	if cmd_lst[0] == 'exit':
+		sys.exit()
+	elif cmd_lst[0] == 'buy':
+		if cmd_lst[1] == 'hops':
+			try:
+				quantity = int(cmd_lst[2])
+			except:
+				print "Insert valid quantity"
+			cost = prices[1] * quantity
+ 			if money > cost:	
+				money -= cost
+				hops += quantity
+				
+
+def char_inserter(chr_length, char = " "):
+	spaces = ""	
+	for row in range(0,chr_length):
+		spaces += char
+	return spaces
+
+# The row system needs fixing
+def user_interface():
+	for row in range(0,100):
+		print ""
+	print_resources()
+	print_prices()
+
+def print_resources():
+	title = "RESOURCES"
+	spaces = char_inserter((51-len(title))/2)
+
+	print spaces + title + spaces
+	print "8" + char_inserter(49,"-") + "8"
+
+	for row in [0,1,2]:
+	  beer_line = beers[row].name + ": " + str(inventory[row])
+	  other_line = ""
+	  if row == 0: other_line = "Malt: " + str(malt)
+	  if row == 1: other_line = "Hops: " + str(hops)
+	  if row == 2: other_line = "Cash: " + str(money)
+
+	  spaces = char_inserter(49-len(beer_line)-len(other_line))
+	  
+	  print " " + beer_line + spaces + other_line
+
+	print "8" + char_inserter(49,"-") + "8"
+
+def print_prices():
+	title = "PRICES"
+	spaces = char_inserter((51-len(title))/2)
+
+	print spaces + title + spaces
+	print "8" + char_inserter(49,"-") + "8"
+	
+	for beer in beers:
+		print " " + beer.name + ": " + str(beer.price)
+
+	print "8" + char_inserter(49,"-") + "8"
+
+	for i in range(0,len(prices)):
+		print " " + str(products[i]) + ": " + str(prices[i])
+	
+def print_brewing():
+	title = "BREW"
+	spaces = char_inserter((51-len(title))/2)
+	
+	print " Slots: " + production_slots
+	for beer in queue:
+		print ""
+
+def game_start(days):
+	while True:
+		user_interface()
+		cmd = raw_input('( -.-) ')
+		parse_command(cmd)
+		
+game_start(20)
